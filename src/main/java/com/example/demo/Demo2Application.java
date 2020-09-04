af0a266825e787ac6eb43d91a763c9fc000cdeb4
Pan Zając <panzajac@localhost.localdomain>
2020-09-04 23:39:12 +0200

    Remove dead code, reformat files, clean up dependencies

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -1,63 +1,36 @@
 package com.example.demo;
 
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-import java.util.Scanner;
-
-// TODO: clean up unused imports
-import com.binance.api.client.BinanceApiRestClient;
-import com.binance.api.client.BinanceApiClientFactory;
-import com.binance.api.client.domain.market.Candlestick;
-import com.binance.api.client.domain.market.CandlestickInterval;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.simp.stomp.StompSessionHandler;
-import org.springframework.web.socket.client.WebSocketClient;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
-
-
 import com.binance.api.client.BinanceApiClientFactory;
 import com.binance.api.client.BinanceApiRestClient;
 import com.binance.api.client.BinanceApiWebSocketClient;
-import com.binance.api.client.domain.market.AggTrade;
-import com.binance.api.client.domain.market.Candlestick;
-import com.binance.api.client.domain.market.CandlestickInterval;
-import com.binance.api.client.domain.market.OrderBook;
-import com.binance.api.client.domain.market.OrderBookEntry;
+import com.binance.api.client.domain.market.*;
 import com.example.demo.services.CandlestickCheckService;
-import com.example.demo.web.MyStompSessionHandler;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Demo2Application {
-	
-	private static String URL = "wss://stream.binance.com:9443/ws/btcusdt@trade";
-	
-	public static void main(String[] args) {
-		//SpringApplication.run(Demo2Application.class, args);
-		CandlestickCheckService candlestickCheckService = new CandlestickCheckService();
-		BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance();
-		BinanceApiRestClient client = factory.newRestClient();
-		client.ping();
-		long serverTime = client.getServerTime();
-		//System.out.println(serverTime);
-		OrderBook orderBook = client.getOrderBook("YFIBTC", 10);
-		List<OrderBookEntry> asks = orderBook.getAsks();
-		OrderBookEntry firstAskEntry = asks.get(0);
-		System.out.println(firstAskEntry.getPrice() + " / " + firstAskEntry.getQty());
-		List<AggTrade> aggTrades = client.getAggTrades("YFIBTC");
-		System.out.println(aggTrades.size());
-		List<Candlestick> candlesticks = client.getCandlestickBars("YFIBTC", CandlestickInterval.TWO_HOURLY);
-		System.out.println(candlesticks.size());
-		boolean shouldBuy = candlestickCheckService.checkIfLastTwoCandlesticksGreen(candlesticks);
-		BinanceApiWebSocketClient client2 = BinanceApiClientFactory.newInstance().newWebSocketClient();
-	}
+
+    private static final String URL = "wss://stream.binance.com:9443/ws/btcusdt@trade";
+
+    public static void main(String[] args) {
+        //SpringApplication.run(Demo2Application.class, args);
+        CandlestickCheckService candlestickCheckService = new CandlestickCheckService();
+        BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance();
+        BinanceApiRestClient client = factory.newRestClient();
+        client.ping();
+        long serverTime = client.getServerTime();
+        //System.out.println(serverTime);
+        OrderBook orderBook = client.getOrderBook("YFIBTC", 10);
+        List<OrderBookEntry> asks = orderBook.getAsks();
+        OrderBookEntry firstAskEntry = asks.get(0);
+        System.out.println(firstAskEntry.getPrice() + " / " + firstAskEntry.getQty());
+        List<AggTrade> aggTrades = client.getAggTrades("YFIBTC");
+        System.out.println(aggTrades.size());
+        List<Candlestick> candlesticks = client.getCandlestickBars("YFIBTC", CandlestickInterval.TWO_HOURLY);
+        System.out.println(candlesticks.size());
+        boolean shouldBuy = candlestickCheckService.checkIfLastTwoCandlesticksGreen(candlesticks);
+        BinanceApiWebSocketClient client2 = BinanceApiClientFactory.newInstance().newWebSocketClient();
+    }
 }
--- src/main/java/com/example/demo/services/CandlestickCheckService.java
+++ src/main/java/com/example/demo/services/CandlestickCheckService.java
@@ -1,23 +1,21 @@
 package com.example.demo.services;
 
-import java.util.List;
-
+import com.binance.api.client.domain.market.Candlestick;
 import org.springframework.stereotype.Service;
 
-import com.binance.api.client.domain.market.Candlestick;
+import java.util.List;
 
 @Service
 public class CandlestickCheckService {
-	
-	public boolean checkIfLastTwoCandlesticksGreen(List<Candlestick> candlesticks) {
-		Candlestick firstCandle = candlesticks.get(candlesticks.size() - 2);
-		Candlestick secondCandle = candlesticks.get(candlesticks.size() - 1);
-		
-		if(checkIfCandleGreen(firstCandle) && checkIfCandleGreen(secondCandle)) return true;
-		else return false;
-	}
-	
-	private boolean checkIfCandleGreen(Candlestick candle) {
-		return (Integer.parseInt(candle.getHigh()) > Integer.parseInt(candle.getOpen())) ? true : false;
-	}
+
+    public boolean checkIfLastTwoCandlesticksGreen(List<Candlestick> candlesticks) {
+        Candlestick firstCandle = candlesticks.get(candlesticks.size() - 2);
+        Candlestick secondCandle = candlesticks.get(candlesticks.size() - 1);
+
+		return checkIfCandleGreen(firstCandle) && checkIfCandleGreen(secondCandle);
+    }
+
+    private boolean checkIfCandleGreen(Candlestick candle) {
+        return Integer.parseInt(candle.getHigh()) > Integer.parseInt(candle.getOpen());
+    }
 }
