5c228e6173277edbca844d5f63478ac633ad4ef1
zajac02020 <redacted>
2020-09-10 12:53:38 +0200

    Implement green bar logic as an Indicator

--- src/main/java/com/example/demo/services/CandlestickCheckService.java
+++ src/main/java/com/example/demo/services/CandlestickCheckService.java
@@ -1,20 +1,13 @@
 package com.example.demo.services;
 
-import com.binance.api.client.domain.market.Candlestick;
+import com.example.demo.indicators.GreenBarsIndicator;
 import org.springframework.stereotype.Service;
-import org.ta4j.core.Bar;
 import org.ta4j.core.BarSeries;
 
 @Service
 public class CandlestickCheckService {
 
-    public boolean checkIfLastTwoBarsGreen(BarSeries barSeries) {
-        int barsCount = barSeries.getBarCount();
-        if (barsCount < 2) {
-            return false;
-        }
-        Bar x = barSeries.getBar(barsCount - 1);
-        Bar y = barSeries.getBar(barsCount - 2);
-        return x.isBullish() && y.isBullish();
-    }
+  public boolean checkIfLastTwoBarsGreen(BarSeries barSeries) {
+    return new GreenBarsIndicator(barSeries, 2).getValue(barSeries.getBarCount() - 1);
+  }
 }
